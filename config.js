0835d79f1e66679d3bb97e92ea62f84bcb125fc9
Charuka1 <129839125+Charuka1@users.noreply.github.com>
2024-12-14 12:14:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '', // ENTER YOUR SESSION_ID TO THIS.
-MONGODB_URI: process.env.MONGODB_URI || '', // ENTER YOUR MONGODB URL TO THIS.
+SESSION_ID: process.env.SESSION_ID || 'DIMENSION-X~3AFkxQLK#4kSknlqpPrJSltspme6NMZfT55yKzSGyioHcYue3DAU', // ENTER YOUR SESSION_ID TO THIS.
+MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net"', // ENTER YOUR MONGODB URL TO THIS.
 
 
 
